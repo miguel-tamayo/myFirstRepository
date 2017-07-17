8b2874b0d918f62af0b6aa145fbe0c61e0a4e523
lancedolan <lance@edlio.com>
2017-07-17 16:15:51 -0500

    Update firstProject.js

--- firstProject.js
+++ firstProject.js
@@ -3,6 +3,6 @@ console.log("Miguel's favorite color is orange");
 console.log( 'green' ) ;
 
 //now I'm going to print out MY OWN numbers...
-console. 1 2 3 4 
+console.log("1 2 3 4");
 
 console.log("2 34 56");
